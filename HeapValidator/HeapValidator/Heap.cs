b4e36cb848ccef2dc17b2ea0b1b8c437fb4b3e01
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-04-16 22:34:55 -0500

    Preliminary setup

---
+++ HeapValidator/HeapValidator/Heap.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeapValidator
+{
+    public class Heap
+    {
+        private int[] HeapArray;
+        private int Size;
+        private int Root;
+
+        public Heap() { }
+
+        public Heap(int[] heapArray, int size)
+        {
+            HeapArray = heapArray;
+            Size = size;
+            Root = HeapArray[0];
+        }
+
+        public bool Validate()
+        {
+            bool minHeap = false;
+            bool maxHeap = false;
+            bool invalid = false;
+
+            int smallest = HeapArray.Min();
+            int largest = HeapArray.Max();
+
+            //Check that root is either the smallest or largest value in the array
+            if (smallest.Equals(Root))
+            {
+                minHeap = true;
+            }
+            else if (largest.Equals(Root))
+            {
+                maxHeap = true;
+            }
+            else
+            {
+                invalid = true;
+            }
+
+            //Now we check to make sure each element in the array is valid for the type of heap it is
+            if (minHeap)
+            {
+
+            }
+            else if (maxHeap)
+            {
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
